18752ed3b1626d9c3623b967e4b8e3e8150dd16f
Ryan Bradley <bradleyrp@gmail.com>
2017-05-15 16:45:41 -0400

    testing naming

--- omni/maps.py
+++ omni/maps.py
@@ -617,7 +617,9 @@ class SliceMeta:
 				#---...once will cause slices to be written with those names, so you should not change it
 				#---! is this OKAY?
 				short_name = self.work.namer.short_namer(new_slice['sn'])
-				new_slice['sn_prefixed'] = self.work.raw.prefixer(short_name)
+				#---! LOOK HERE THIS IS A HACK!!!!
+				try: new_slice['sn_prefixed'] = self.work.raw.prefixer(short_name)
+				except: new_slice['sn_prefixed'] = self.work.raw.prefixer(new_slice['sn'])
 				spotname = self.work.raw.spotname_lookup(new_slice['sn'])
 				new_slice['tpr_keyfinder'] = self.work.raw.keyfinder((spotname,'tpr'))
 				new_slice['traj_keyfinder'] = self.work.raw.keyfinder(
@@ -751,7 +753,9 @@ class ParsedRawData:
 		#---failure to run the shortnamer just passes the full simulation name
 		except: sn = sn_full
 		assert type(sn)==str
-		spotnames = [key for key,val in self.toc.items() if sn in val]
+		#---the following or statement allows this to work for both full and short names
+		#---! this needs further testing
+		spotnames = [key for key,val in self.toc.items() if sn in val or sn_full in val]
 		if not spotnames: 
 			#---in case top diverges from prefixer we check the regexes
 			top_regexes = [v['regexes']['top'] for v in self.work.paths['spots'].values()]
@@ -775,10 +779,10 @@ class ParsedRawData:
 		#---we include the partname when accessing self.spots
 		try: this_spot = self.spotname_lookup(sn)
 		except: raise Exception('cannot find the spot for %s'%sn)
-		#try:
-		spot = spotname,partname = (this_spot,self.trajectory_format)
-		prefix = self.spots[spot]['namer'](spotname,sn)
-		#except: raise Exception('[ERROR] prefixer failure on simulation "%s" (check your namer)'%sn)
+		try:
+			spot = spotname,partname = (this_spot,self.trajectory_format)
+			prefix = self.spots[spot]['namer'](spotname,sn)
+		except: raise Exception('[ERROR] prefixer failure on simulation "%s" (check your namer)'%sn)
 		return prefix
 
 	def treeparser(self,spot,**kwargs):

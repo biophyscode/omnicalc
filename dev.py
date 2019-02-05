52226f4a6cfddb6f70a59316548a1d8c48899df3
Ryan Bradley <bradleyrp@gmail.com>
2019-02-04 20:23:53 -0500

    developed a generic loop expander

--- dev.py
+++ dev.py
@@ -9,7 +9,7 @@ from ortho import json_type_fixer
 from ortho import Handler
 from ortho import treeview
 from ortho import catalog
-from ortho import delveset
+from ortho import delveset,delve
 
 # put this in config?
 old_config = {'spots': {'sims': {'namer': 'lambda name,spot=None: name',
@@ -192,14 +192,6 @@ posts = dict([(k,list(filter(lambda x:x.kind==k,post_files_resolved)))
 
 that = posts['datspec'][0]
 
-"""
-next steps:
-	refactor actually completely rewrite the thing that interprets the yaml files
-	anticipate the data in a calculation result
-	compare to the posts['datspect'] to generate a list of pending jobs
-		the code to do this resolution will be complex
-"""
-
 class Specs:
 	def __init__(self,spot):
 		self.spot = os.path.abspath(os.path.expanduser(spot))
@@ -224,10 +216,13 @@ class Specs:
 		for fn in sources:
 			sources_unravel[fn] = list(catalog(sources[fn]))
 		# perfect check for unique paths
-		# note that this seems expensive but only taks 0.0017s then 0.0062s with the merge at the end
-		paths_all = [tuple(i) for j in [list(zip(*i))[0] for i in sources_unravel.values()] for i in j]
-		# note that this exception does not care if you have e.g. slices: {} and a nonempty slices elsewhere
-		#   which is the desired behavior. we only want to check for paths that overwrite each other
+		# note that this seems expensive but only taks 0.0017s 
+		#   and then 0.0062s with the merge at the end
+		paths_all = [tuple(i) for j in 
+			[list(zip(*i))[0] for i in sources_unravel.values()] for i in j]
+		# note that this exception does not care if you have e.g. slices: {} 
+		#   and a nonempty slices elsewhere which is the desired behavior. 
+		#   we only want to check for paths that overwrite each other
 		#! ignore paths that overwrite with the same value
 		if len(paths_all)!=len(set(paths_all)):
 			from collections import Counter
@@ -243,3 +238,161 @@ class Specs:
 #! hardcoded path below	
 specs = Specs(spot='~/omicron/factory/calc/ptdins/calcs/specs')
 
+# STEP 5: build a list of calculation dependencies 
+
+calcs_meta = specs.raw['calculations']
+
+def infer_calculation_order(calcs_meta):
+	"""
+	Resolve dependencies in the calculation list.
+	Note that this function is a verbatim port from original omnicalc.
+	This function encodes the data structure implicitly, and should be 
+	replaced with a more modular version.
+	"""
+	upstream_catalog = [i for i,j in catalog(calcs_meta) if 'upstream' in i]
+	# if there are no specs required to get the upstream data object the user can either 
+	#   use none/None as a placeholder or use the name as the key as in "upstream: name"
+	for uu,uc in enumerate(upstream_catalog):
+		if uc[-1]=='upstream': upstream_catalog[uu] = upstream_catalog[uu]+[delve(calcs_meta,*uc)]
+	depends = {}
+	# formulate a list of dependencies while accounting for multiple upstream dependencies
+	for t in upstream_catalog:
+		if t[0] not in depends: depends[t[0]] = []
+		depends[t[0]].extend([t[ii+1] 
+			for ii,i in enumerate(t) 
+			if ii<len(t)-1 and t[ii]=='upstream'])
+	calckeys = [i for i in calcs_meta if i not in depends]
+	# if the calculation uses an upstream list instead of dictionary we flatten it
+	depends = dict([(k,(v if not all([type(i)==list for i in v]) else 
+		[j for k in v for j in k])) for k,v in depends.items()])
+	# check that the calckeys has enough elements 
+	_ = list(set(calckeys+[i for j in depends.values() for i in j]))
+	# paranoid security check for infinite loop. you cannot use popitem
+	time_limit = 10 
+	depends = list(depends.items())
+	start_time = time.time()
+	while any(depends):
+		ii,i = depends.pop()
+		if all([j in calckeys for j in i]) and i!=[]: calckeys.append(ii)
+		else: depends.insert(0,(ii,i))
+		if time.time()>(start_time+time_limit): 
+			raise Exception(
+				'It is taking too long to resolve the dependency graph. '
+				'You might have a loop in your graph of dependencies.')
+	return calckeys
+
+calc_order = infer_calculation_order(calcs_meta)
+
+"""
+next steps:
+	refactor actually completely rewrite the thing that interprets the yaml files
+	anticipate the data in a calculation result
+	compare to the posts['datspect'] to generate a list of pending jobs
+		the code to do this resolution will be complex
+sidetracked:
+	started working on the above and realized it would be nice to not hard-code the relationships between calculations
+	idea: make all relations into calss relations
+		basically each item in a calculation gets slotted into a class object and we can subclass 
+		and inherit to make larger classes
+		this is somewhat the opposite of abstraction
+		the problem is that abstraction requires handlers that require hard-coding
+		so the handlers themselves are no longer abstract
+		basically, a bunch of python classes might be a better abstraction mechanism
+		than a two-level meta-class kind of thing where I write the handler in some dictionary
+		then write a meta-handler to turn that into a set of code
+		basically the python code is both the data structure and the handler
+		anyway, this sounds like I'm chiefing over here so time to get to work
+		the Handler parent class already does this a lot so I will start there
+		the handler can be the interpreter of the calculations and then code up the relations too
+		note that we also need some kind of nesting
+	needs for Handler
+		fix the name issue soon!
+			consider removing name so everything is totally generic
+			this might require a lot of adjustment in other parts of the code?
+			possibly needs a switch to handle it
+		ensure that when we look at a Handler subclass instance 
+			we can tell what is "novel" and what is part of the base class 
+			i.e. if I set self.specs inside the method, how can I distinguish it from self.name
+commit 
+"""
+
+class Specs(Handler):
+	def upstream(self,upstream,**kwargs):
+		self.upstream = upstream
+		self.default(**kwargs)
+		return "I AM UPSTREAM"
+	def default(self,**kwargs):
+		self.raw = kwargs
+		# resolve loops here ...!!!
+
+class Calculation(Handler):
+	def item(self,slice_name,collections,specs,group=None,uptype='post'):
+		#! this is almost an edge in our calculation
+		self.specs = Specs(**specs)
+
+class CalculationSet(Handler):
+	#! you cannot name the function _item below because of handler
+	def item(self,many):
+		self.many = many
+		for key,val in self.many.items():
+			self.many[key] = Calculation(**val)
+
+them = CalculationSet(**dict(many=calcs_meta))
+that = them.many['lipid_abstractor']
+print(that.specs)
+
+"""
+resolving loop structures
+	each Calculation object has a self.raw attribute which could have loops in it
+	each time we encounter a loop, we need to make a copy of everything "above"
+	and then each copy gets a different value for everything "below"
+"""
+
+import copy,itertools
+
+def resolve_loops(tree):
+	"""
+	Generic function for resolving the "loop" keyword in a nested dictionary.
+	The loop keyword can point to a dictionary or a list of values.
+	In both cases, the product of all values (if dict) or list items (if list)
+	is generated and a unique tree is produced for each combination in the 
+	product.
+	#! what to do about keys?
+	"""
+	paths = list(catalog(tree))
+	# get the pivots for the loops
+	loop_spots = set([tuple(path[:path.index('loop')+2]) 
+		for path,val in paths if 'loop' in path])
+	loop_full = set([tuple(path) for path,val in paths if 'loop' in path])
+	loop_term = [i for i in loop_full if i[-1]=='loop']
+	# collect all terminal loop values organized by path
+	loop_term_values = dict([(i[:-1],delve(tree,*i)) for i in loop_term])
+	splits = {}
+	# the non-terminal loops have full dictionaries
+	for item in loop_full:
+		if item[-1]=='loop': 
+			downstream = delve(tree,*item)
+			if (not isinstance(downstream,dict) 
+				and not isinstance(downstream,list)):
+				raise Exception(
+					('Cannot loop the following path: %s with value: %s')%(
+						item,str(downstream)))
+			splits[item[:-1]] = downstream
+	for item in loop_spots:
+		if item[-2]=='loop': 
+			path = item[:item.index('loop')]
+			tail = item[:item.index('loop')+2]
+			if path not in splits: splits[path] = []
+			splits[path].append(delve(tree,*tail))
+	combos = [dict(zip(splits.keys(),i)) 
+		for i in list(itertools.product(*splits.values()))]
+	if not combos: return dict(multi=[copy.deepcopy(tree)])
+	multiplexed = []
+	for mod in combos:
+		this = copy.deepcopy(tree)
+		for key,val in mod.items():
+			delveset(this,*key,value=val)
+		multiplexed.append(this)
+	return dict(multi=multiplexed)
+
+multi = resolve_loops(that.specs.raw)
